4e8d6162974c7ca3785680293aec09642d1ccec1
takubokudori <nockrule@yahoo.co.jp>
2019-11-13 21:37:36 +0900

    Fix mitame

--- LineFormatter/ProxyForm.Designer.cs
+++ LineFormatter/ProxyForm.Designer.cs
@@ -47,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 61);
+            this.label1.Location = new System.Drawing.Point(76, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 24);
             this.label1.TabIndex = 1;
@@ -56,7 +56,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 121);
+            this.label2.Location = new System.Drawing.Point(73, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 24);
             this.label2.TabIndex = 2;
@@ -79,7 +79,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 179);
+            this.label3.Location = new System.Drawing.Point(23, 176);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 24);
             this.label3.TabIndex = 5;
